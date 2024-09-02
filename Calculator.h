c3f5f5ff324382a3cf13c79db31d86b120ce3b0b
Hannah Nguyen <hannahnguyen@ucsb.edu>
2024-09-02 02:18:25 -0700

    Updated Header file
    Added new function ‘double calculate_expression(double num1, double num2, std::string sign)’, which was created to assist with calculating over 2 numbers in a single expression. Removed boolean argument from ‘double calculate(string user_input);’, since it was unnecessary.

--- Calculator.h
+++ Calculator.h
@@ -7,6 +7,7 @@
 
 using namespace std;
 double calculate(string user_input);
+double calculate_expression(double num1, double num2, std::string sign);
 void help();
 void update_log();
 void history_log();
