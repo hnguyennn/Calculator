5894a8352db8b218e93b2de600e1c0ecb774c658
Hannah Nguyen <hannahnguyen@ucsb.edu>
2024-09-06 01:46:14 -0700

    Added member function declarations to the file
    Declared the constructor, destructor, and many other functions in this file. Added comments to describe the purpose of each function and some of the function logic.

--- History_Log.cpp
+++ History_Log.cpp
@@ -1,3 +1,30 @@
 // History_Log.cpp
 #include "Calculator.h"
 
+
+History_Log::History_Log(){
+// Constructor. Creates and initializes the history log.
+}
+
+History_Log::~History_Log(){
+// Destructor, uses clear() and then deletes the head. Completely erases the history log.
+}
+
+History_Log* History_Log::insert_into_memory(std::string expression){
+// Creates a new node for the expressoin and adds the new expression at the end of the linked list (tail). 
+// Returns a pointer to this new node. If it hits the max amount of nodes (expressions), output an error message.
+}
+
+size_t History_Log::get_size(){
+// Gets the total number of nodes (expressions) that the history log currently holds. Will cap around a certain amount.
+    return 0;
+}
+
+void History_Log::clear(){
+// Resets the history log to 0 nodes (expressions).
+}
+
+void History_Log::print_memory(){
+// Outputs all expressions, starting from oldest (head) to newest (tail).
+}
+
