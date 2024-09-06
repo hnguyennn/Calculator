c723a566af9958387b48682bee016ed5435ed456
Hannah Nguyen <hannahnguyen@ucsb.edu>
2024-09-06 01:42:00 -0700

    Modified header file, ‘Calculator.h’
    Added the declaration of the class, ‘History_Log’ to it, along with the necessary members and member functions. This will be used by the ‘History_Log.cpp’ file.

--- Calculator.h
+++ Calculator.h
@@ -4,7 +4,7 @@
 #define Calculator_h
 #include <string>
 
-
+//Calculator.cpp
 using namespace std;
 double calculate(string user_input);
 double calculate_expression(double num1, double num2, std::string sign);
@@ -13,5 +13,23 @@ void update_log();
 void history_log();
 void program();
 
+//History_Log.cpp
+class History_Log {
+private:
+    struct Node {
+        std::string data;
+        Node *next;
+        Node(std::string s = "") : data(s), next(nullptr) {}
+    };
+    Node *_head, *_tail, *_prev_to_current;
+    size_t _size = 0;
+public:
+    History_Log();
+    ~History_Log();
+    History_Log *insert_into_memory(std::string expression);
+    size_t get_size();
+    void clear();
+    void print_memory();
+};
 
 #endif /* Calculator_h */
