fea4808739d9645906229d0a02207ef9f1f196e6
Hannah Nguyen <hannahnguyen@ucsb.edu>
2024-12-29 01:29:37 -0800

    Added new html pages and updated Flask with new routes
    Added the html pages for each of the 4 options and modified app.py to have routes to them.

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('calculator.html')
 
+@app.route('/calculate')
+def calculate():
+    return render_template('calculate.html')  
+
+@app.route('/help')
+def help():
+    return render_template('help.html')  
+
+@app.route('/history-log')
+def history_log():
+    return render_template('history_log.html')  
+
+@app.route('/update-log')
+def update_log():
+    return render_template('update_log.html')  
+
 if __name__ == "__main__":
     app.run(debug=True)
